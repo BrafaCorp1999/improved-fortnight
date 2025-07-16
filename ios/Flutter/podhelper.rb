6db4b3c5d6a685da3093e835a53e63832d8be3fc
Brayan Rafael Condori Chambi <148501491+BrafaCorp1999@users.noreply.github.com>
2025-07-16 12:51:11 -0400

    Update podhelper.rb

--- ios/Flutter/podhelper.rb
+++ ios/Flutter/podhelper.rb
@@ -1,3 +1,6 @@
-#!/bin/bash
+# Este es el archivo podhelper.rb que se usa para configurar Flutter con CocoaPods
+
 flutter_application_path = '../'
+
+# Asegúrate de que esta ruta sea válida en tu proyecto. Si no lo es, debes ajustarla.
 load File.join(flutter_application_path, '.ios', 'Flutter', 'podhelper.rb')
